b48aec014d9acefc8e4ced53fb238326aa7f3708
RPullia <robertopullia@gmail.com>
2024-01-19 09:38:44 +0100

    applied stashed changes on fraud microservice, added logback

--- fraud/src/main/java/com/robp/fraud/FraudController.java
+++ fraud/src/main/java/com/robp/fraud/FraudController.java
@@ -1,5 +1,7 @@
 package com.robp.fraud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class FraudController {
    private final FraudCheckService fraudCheckService;
 
+   private static final Logger logger = LoggerFactory.getLogger(FraudController.class);
+
    public FraudController(FraudCheckService fraudCheckService){
        this.fraudCheckService = fraudCheckService;
    }
     @GetMapping(path ="{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
        boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+       logger.info("Fraud check request for customer: ", customerId);
        return new FraudCheckResponse(isFraudulentCustomer);
     }
 
